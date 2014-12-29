222262e8ff9ff9a2908473fa9abefff9078575e3
Patryk <patrickus@wp.pl>
2014-12-29 14:43:54 +0100

    LXIV

---
+++ czynsze/DataAccess/IItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace czynsze.DataAccess
+{
+    interface IItem
+    {
+        void Add(string[] record);
+        void Edit(string[] record);
+        void Remove(string[] record);
+        void Set(string[] record);
+        string Validate(string[] record);
+    }
+}
