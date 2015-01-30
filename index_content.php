8adb8977ec11ed51c927ddebfa12def528f0afd0
Patryk <patrickus@wp.pl>
2015-01-30 14:19:59 +0100

    IX

--- index_content.php
+++ index_content.php
@@ -1,11 +1,9 @@
 <?php
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+?>
 
-$content = "index_content.php";
-
-include 'master.php';
+<p>Strona startowa</p>
