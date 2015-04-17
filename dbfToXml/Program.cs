f005ecfb6961907aeeed8251584d5582dcb37ace
Patryk <patrickus@wp.pl>
2015-04-17 14:06:43 +0200

    II

--- dbfToXml/Program.cs
+++ dbfToXml/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Data.OleDb;
 using System.Data;
 using System.Xml;
+using System.IO;
 
 namespace dbfToXml
 {
@@ -63,30 +64,123 @@ namespace dbfToXml
                 foreach (DataRow dataRow in table.Rows)
                 {
                     object[] itemArray = dataRow.ItemArray;
-                    SaveItemToXml("akronim", itemArray[columnNameToOrdinal["indeks_kon"]], true);
-                    SaveItemToXml("nip", itemArray[columnNameToOrdinal["nr_ident"]], true);
-                    SaveItemToXml("numer", itemArray[columnNameToOrdinal["nr_fk"]], true);
+                    string kod, miasto, ulica;
+                    string fileName = "1.xml";
+
+                    SaveItemToXml("akronim", itemArray[columnNameToOrdinal["indeks_kon"]]);
+                    SaveItemToXml("nip", itemArray[columnNameToOrdinal["nr_ident"]]);
+                    SaveItemToXml("numer", itemArray[columnNameToOrdinal["nr_fk"]]);
                     SaveItemToXml("kwota_plat", itemArray[columnNameToOrdinal["wartosc"]], false);
                     SaveItemToXml("kwota_pln_plat", itemArray[columnNameToOrdinal["wartosc"]], false);
+                    SaveItemToXml("nazwa1", itemArray[columnNameToOrdinal["platnik"]]);
+                    SaveItemToXml("nazwa2", itemArray[columnNameToOrdinal["platnik1"]]);
+
+                    ParseAddress(String.Format("{0};{1}", itemArray[columnNameToOrdinal["adres1"]], itemArray[columnNameToOrdinal["adres2"]]), out kod, out miasto, out ulica);
+                    SaveItemToXml("kod_pocztowy", kod);
+                    SaveItemToXml("miasto", miasto);
+                    SaveItemToXml("miasto", miasto);
+
+                    SaveItemToXml("fiskalna", BoolToPolishBool(itemArray[columnNameToOrdinal["fisk"]]), false);
+                    SaveItemToXml("root/rejestry_sprzedazy_vat/rejestr_sprzedazy_vat/eksport", BoolToPolishBool(itemArray[columnNameToOrdinal["exported"]]), false);
+
+                    xmlDocument.Save(fileName);
+
+                    string xml;
+
+                    using (StreamReader streamReader = new StreamReader(fileName, Encoding.GetEncoding("Windows-1250")))
+                        xml = streamReader.ReadToEnd();
+
+                    using (StreamWriter streamWriter = new StreamWriter(fileName))
+                        streamWriter.Write(xml.Replace("&lt;", "<").Replace("&gt;", ">"));
 
                     break;
                 }
-
-                xmlDocument.Save("1.xml");
             }
         }
 
-        static void SaveItemToXml(string tagName, object value, bool includeCDataHashes)
+        static void SaveItemToXml(string tagNameOrXPath, object value, bool includeCDataHashes = true)
         {
-            tagName = tagName.ToUpper();
-            XmlNodeList xmlNodeList = xmlDocument.GetElementsByTagName(tagName);
+            tagNameOrXPath = tagNameOrXPath.ToUpper();
+            XmlNodeList xmlNodeList;
             string valueString = value.ToString().Trim();
+
+            if (tagNameOrXPath.Contains('/'))
+                xmlNodeList = xmlDocument.SelectNodes(tagNameOrXPath);
+            else
+                xmlNodeList = xmlDocument.GetElementsByTagName(tagNameOrXPath);
 
             if (includeCDataHashes)
                 valueString = String.Format("<![CDATA[{0}]]>", valueString);
 
             foreach (XmlNode xmlNode in xmlNodeList)
                 xmlNode.InnerText = valueString;
+        }
+
+        static void ParseAddress(string adres, out string kod, out string miasto, out string ulica)
+        {
+            int firstPartOfPostalCode = 0;
+            int secondPartOfPostalCode = 0;
+            bool postalCodeFound = false;
+            int semicolonIndex = adres.IndexOf(';');
+            int cityIndex = -1;
+
+            for (int i = 0; i < adres.Length && !postalCodeFound; i++)
+                if (Char.IsNumber(adres[i]))
+                    try
+                    {
+                        int dashIndex = adres.IndexOf('-', i + 1);
+                        firstPartOfPostalCode = Int32.Parse(adres.Substring(i, dashIndex - i));
+
+                        for (int j = dashIndex + 1; j < adres.Length - 1; j++)
+                            if (!Char.IsNumber(adres[j + 1]))
+                            {
+                                secondPartOfPostalCode = Int32.Parse(adres.Substring(dashIndex + 1, j - dashIndex + 1));
+                                cityIndex = j + 1;
+                                postalCodeFound = true;
+
+                                break;
+                            }
+                    }
+                    catch { }
+
+            if (postalCodeFound)
+                kod = String.Format("{0}-{1}", firstPartOfPostalCode, secondPartOfPostalCode);
+            else
+                kod = null;
+
+            try
+            {
+                int commaIndex = adres.IndexOf(adres, cityIndex);
+
+                if (commaIndex == -1)
+                    commaIndex = Int32.MaxValue;
+
+                miasto = adres.Substring(cityIndex, new int[] { commaIndex, semicolonIndex, adres.Length }.Where(i => i > cityIndex).Min() - cityIndex).Trim();
+            }
+            catch
+            {
+                miasto = null;
+            }
+
+            try
+            {
+                ulica = adres.Replace(kod, String.Empty).Replace(miasto, String.Empty).Replace(",", String.Empty).Replace(";", String.Empty);
+                ulica = ulica.Substring(ulica.ToLower().IndexOf("ul.")).Trim();
+            }
+            catch
+            {
+                ulica = null;
+            }
+        }
+
+        static string BoolToPolishBool(object value)
+        {
+            bool boolValue = Convert.ToBoolean(value);
+
+            if (boolValue)
+                return "Tak";
+            else
+                return "Nie";
         }
     }
 }
