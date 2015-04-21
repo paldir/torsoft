6927504935f69113452cc57f00da32981927d5de
Patryk <patrickus@wp.pl>
2015-04-21 11:53:08 +0200

    IV

--- dbfToXml/Program.cs
+++ dbfToXml/Program.cs
@@ -13,36 +13,42 @@ namespace dbfToXml
 {
     class Program
     {
-        static XmlDocument xmlDocument;
+        static XmlDocument dokumentXml;
         //static object[] itemArray;
         //static Dictionary<string, int> columnNameToOrdinal;
 
         static void Main(string[] args)
         {
-            using (OleDbConnection connection = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\paldir\Documents\GitHub\dbfToXmlConverter\dbfToXml\bin\Debug\DBF;Extended Properties=dBASE IV;"))
+            using (OleDbConnection połączenie = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\paldir\Documents\GitHub\dbfToXmlConverter\dbfToXml\bin\Debug\DBF;Extended Properties=dBASE IV;"))
             {
-                connection.Open();
+                połączenie.Open();
 
-                OleDbCommand fkCommand = new OleDbCommand("SELECT * FROM _FK.DBF", connection);
-                OleDbCommand fkListCommand = new OleDbCommand("SELECT * FROM _FK_LIST.DBF", connection);
-                DataSet fkDataSet = new DataSet();
-                DataSet fkListDataSet = new DataSet();
-                OleDbDataAdapter fkDataAdapter = new OleDbDataAdapter(fkCommand);
-                OleDbDataAdapter fkListDataAdapter = new OleDbDataAdapter(fkListCommand);
+                DataTable fk;
+                DataTable fkList;
 
-                fkDataAdapter.Fill(fkDataSet);
-                fkListDataAdapter.Fill(fkListDataSet);
+                {
+                    OleDbCommand komendaFk = new OleDbCommand("SELECT * FROM _FK.DBF", połączenie);
+                    OleDbCommand komendaFkList = new OleDbCommand("SELECT * FROM _FK_LIST.DBF", połączenie);
+                    DataSet daneFk = new DataSet();
+                    DataSet daneFkList = new DataSet();
+                    OleDbDataAdapter adapterFk = new OleDbDataAdapter(komendaFk);
+                    OleDbDataAdapter adapterFkList = new OleDbDataAdapter(komendaFkList);
 
-                DataTable fkTable = fkDataSet.Tables[0];
-                DataTable fkListTable = fkListDataSet.Tables[0];
-                Dictionary<string, int> fkColumnNameToOrdinal = new Dictionary<string, int>();
-                Dictionary<string, int> fkListColumnNameToOrdinal = new Dictionary<string, int>();
+                    adapterFk.Fill(daneFk);
+                    adapterFkList.Fill(daneFkList);
 
-                foreach (DataColumn column in fkTable.Columns)
-                    fkColumnNameToOrdinal.Add(column.ColumnName.ToLower(), column.Ordinal);
+                    fk = daneFk.Tables[0];
+                    fkList = daneFkList.Tables[0];
+                }
 
-                foreach (DataColumn column in fkListTable.Columns)
-                    fkListColumnNameToOrdinal.Add(column.ColumnName.ToLower(), column.Ordinal);
+                Dictionary<string, int> nazwaKolumnyDoJejNumeruFk = new Dictionary<string, int>();
+                Dictionary<string, int> nazwaKolumnyDoJejNumeruFkList = new Dictionary<string, int>();
+
+                foreach (DataColumn kolumna in fk.Columns)
+                    nazwaKolumnyDoJejNumeruFk.Add(kolumna.ColumnName.ToLower(), kolumna.Ordinal);
+
+                foreach (DataColumn kolumna in fkList.Columns)
+                    nazwaKolumnyDoJejNumeruFkList.Add(kolumna.ColumnName.ToLower(), kolumna.Ordinal);
 
                 /*using (System.IO.StreamWriter streamWriter = new System.IO.StreamWriter("index.html"))
                 {
@@ -66,121 +72,132 @@ namespace dbfToXml
                     streamWriter.Write("</table></body></html>");
                 }*/
 
-                xmlDocument = new XmlDocument();
+                dokumentXml = new XmlDocument();
 
-                xmlDocument.Load("wzor_fk.xml");
+                dokumentXml.Load("wzor_fk.xml");
 
-                foreach (DataRow dataRow in fkTable.Rows)
+                foreach (DataRow wiersz in fk.Rows)
                 {
-                    object[] itemArray = dataRow.ItemArray;
+                    object[] polaFk = wiersz.ItemArray;
                     string kod, miasto, ulica;
-                    string fileName = "1.xml";
+                    string plik = "1.xml";
+                    double id = (double)polaFk[nazwaKolumnyDoJejNumeruFk["nr_system"]];
 
-                    SaveItemToXml("akronim", itemArray[fkColumnNameToOrdinal["indeks_kon"]]);
-                    SaveItemToXml("nip", itemArray[fkColumnNameToOrdinal["nr_ident"]]);
-                    SaveItemToXml("numer", itemArray[fkColumnNameToOrdinal["nr_fk"]]);
-                    SaveItemToXml("kwota_plat", itemArray[fkColumnNameToOrdinal["wartosc"]], false);
-                    SaveItemToXml("kwota_pln_plat", itemArray[fkColumnNameToOrdinal["wartosc"]], false);
-                    SaveItemToXml("nazwa1", itemArray[fkColumnNameToOrdinal["platnik"]]);
-                    SaveItemToXml("nazwa2", itemArray[fkColumnNameToOrdinal["platnik1"]]);
+                    ZapiszWXml("akronim", polaFk[nazwaKolumnyDoJejNumeruFk["indeks_kon"]]);
+                    ZapiszWXml("nip", polaFk[nazwaKolumnyDoJejNumeruFk["nr_ident"]]);
+                    ZapiszWXml("numer", polaFk[nazwaKolumnyDoJejNumeruFk["nr_fk"]]);
+                    ZapiszWXml("kwota_plat", polaFk[nazwaKolumnyDoJejNumeruFk["wartosc"]], false);
+                    ZapiszWXml("kwota_pln_plat", polaFk[nazwaKolumnyDoJejNumeruFk["wartosc"]], false);
+                    ZapiszWXml("nazwa1", polaFk[nazwaKolumnyDoJejNumeruFk["platnik"]]);
+                    ZapiszWXml("nazwa2", polaFk[nazwaKolumnyDoJejNumeruFk["platnik1"]]);
 
-                    ParseAddress(String.Format("{0};{1}", itemArray[fkColumnNameToOrdinal["adres1"]], itemArray[fkColumnNameToOrdinal["adres2"]]), out kod, out miasto, out ulica);
-                    SaveItemToXml("kod_pocztowy", kod);
-                    SaveItemToXml("miasto", miasto);
-                    SaveItemToXml("miasto", miasto);
+                    AnalizujAdres(String.Format("{0};{1}", polaFk[nazwaKolumnyDoJejNumeruFk["adres1"]], polaFk[nazwaKolumnyDoJejNumeruFk["adres2"]]), out kod, out miasto, out ulica);
+                    ZapiszWXml("kod_pocztowy", kod);
+                    ZapiszWXml("miasto", miasto);
+                    ZapiszWXml("miasto", miasto);
 
-                    SaveItemToXml("fiskalna", BoolToPolishBool(itemArray[fkColumnNameToOrdinal["fisk"]]), false);
-                    SaveItemToXml("root/rejestry_sprzedazy_vat/rejestr_sprzedazy_vat/eksport", BoolToPolishBool(itemArray[fkColumnNameToOrdinal["exported"]]), false);
+                    ZapiszWXml("fiskalna", ZamieńBoolNaPolskąNazwę(polaFk[nazwaKolumnyDoJejNumeruFk["fisk"]]), false);
+                    ZapiszWXml("root/rejestry_sprzedazy_vat/rejestr_sprzedazy_vat/eksport", ZamieńBoolNaPolskąNazwę(polaFk[nazwaKolumnyDoJejNumeruFk["exported"]]), false);
 
-                    DateTime dateTime = (DateTime)itemArray[fkColumnNameToOrdinal["data"]];
-                    string dateTimeString = dateTime.ToShortDateString();
-                    string deadlineString = dateTime.AddDays((double)itemArray[fkColumnNameToOrdinal["termin"]]).ToShortDateString();
-                    SaveItemToXml("data_wystawienia", dateTimeString);
-                    SaveItemToXml("data_dataobowiazkupodatkowego", dateTimeString);
-                    SaveItemToXml("data_dataprawaodliczenia", dateTimeString);
-                    SaveItemToXml("termin", deadlineString);
-                    SaveItemToXml("termin_plat", deadlineString);
+                    DateTime data = (DateTime)polaFk[nazwaKolumnyDoJejNumeruFk["data"]];
+                    string napisDaty = data.ToShortDateString();
+                    string napisTerminu = data.AddDays((double)polaFk[nazwaKolumnyDoJejNumeruFk["termin"]]).ToShortDateString();
+                    ZapiszWXml("data_wystawienia", napisDaty);
+                    ZapiszWXml("data_dataobowiazkupodatkowego", napisDaty);
+                    ZapiszWXml("data_dataprawaodliczenia", napisDaty);
+                    ZapiszWXml("termin", napisTerminu);
+                    ZapiszWXml("termin_plat", napisTerminu);
 
-                    SaveItemToXml("data_sprzedazy", ((DateTime)itemArray[fkColumnNameToOrdinal["data_sprz"]]).ToShortDateString());
+                    ZapiszWXml("data_sprzedazy", ((DateTime)polaFk[nazwaKolumnyDoJejNumeruFk["data_sprz"]]).ToShortDateString());
 
+                    XmlNode pozycje = dokumentXml.GetElementsByTagName("POZYCJE")[0];
+                    XmlNode wzórPozycji = dokumentXml.GetElementsByTagName("POZYCJA")[0];
 
+                    foreach (DataRow wierszFkList in fkList.Rows)
+                    {
+                        object[] polaFkList = wierszFkList.ItemArray;
 
-                    xmlDocument.Save(fileName);
+                        if ((double)polaFkList[nazwaKolumnyDoJejNumeruFkList["nr_system"]] == id)
+                            DodajPozycję(pozycje, wzórPozycji, Convert.ToDecimal(polaFkList[nazwaKolumnyDoJejNumeruFkList["vat"]]), String.Empty, 0, 0, String.Empty, false);
+                    }
+
+                    wzórPozycji.ParentNode.RemoveChild(wzórPozycji);
+                    dokumentXml.Save(plik);
 
                     string xml;
 
-                    using (StreamReader streamReader = new StreamReader(fileName, Encoding.GetEncoding("Windows-1250")))
-                        xml = streamReader.ReadToEnd();
+                    using (StreamReader strumień = new StreamReader(plik, Encoding.GetEncoding("Windows-1250")))
+                        xml = strumień.ReadToEnd();
 
-                    using (StreamWriter streamWriter = new StreamWriter(fileName))
-                        streamWriter.Write(xml.Replace("&lt;", "<").Replace("&gt;", ">"));
+                    using (StreamWriter strumień = new StreamWriter(plik))
+                        strumień.Write(xml.Replace("&lt;", "<").Replace("&gt;", ">"));
 
                     break;
                 }
             }
         }
 
-        static void SaveItemToXml(string tagNameOrXPath, object value, bool includeCDataHashes = true)
+        static void ZapiszWXml(string tagXmlAlboXPath, object wartość, bool dodaćCData = true)
         {
-            tagNameOrXPath = tagNameOrXPath.ToUpper();
-            XmlNodeList xmlNodeList;
-            string valueString = null;
+            tagXmlAlboXPath = tagXmlAlboXPath.ToUpper();
+            XmlNodeList węzły;
+            string napisWartości = null;
 
-            if (value != null)
-                valueString = value.ToString().Trim();
+            if (wartość != null)
+                napisWartości = wartość.ToString().Trim();
 
-            if (tagNameOrXPath.Contains('/'))
-                xmlNodeList = xmlDocument.SelectNodes(tagNameOrXPath);
+            if (tagXmlAlboXPath.Contains('/'))
+                węzły = dokumentXml.SelectNodes(tagXmlAlboXPath);
             else
-                xmlNodeList = xmlDocument.GetElementsByTagName(tagNameOrXPath);
+                węzły = dokumentXml.GetElementsByTagName(tagXmlAlboXPath);
 
-            if (includeCDataHashes)
-                valueString = String.Format("<![CDATA[{0}]]>", valueString);
+            if (dodaćCData)
+                napisWartości = String.Format("<![CDATA[{0}]]>", napisWartości);
 
-            foreach (XmlNode xmlNode in xmlNodeList)
-                xmlNode.InnerText = valueString;
+            foreach (XmlNode węzeł in węzły)
+                węzeł.InnerText = napisWartości;
         }
 
-        static void ParseAddress(string adres, out string kod, out string miasto, out string ulica)
+        static void AnalizujAdres(string adres, out string kod, out string miasto, out string ulica)
         {
-            int firstPartOfPostalCode = 0;
-            int secondPartOfPostalCode = 0;
-            bool postalCodeFound = false;
-            int semicolonIndex = adres.IndexOf(';');
-            int cityIndex = -1;
+            int pierwszaCzęśćKoduPocztowego = 0;
+            int drugaCzęśćKoduPocztowego = 0;
+            bool kodPocztowyZnaleziony = false;
+            int indeksŚrednika = adres.IndexOf(';');
+            int indeksMiasta = -1;
 
-            for (int i = 0; i < adres.Length && !postalCodeFound; i++)
+            for (int i = 0; i < adres.Length && !kodPocztowyZnaleziony; i++)
                 if (Char.IsNumber(adres[i]))
                     try
                     {
-                        int dashIndex = adres.IndexOf('-', i + 1);
-                        firstPartOfPostalCode = Int32.Parse(adres.Substring(i, dashIndex - i));
+                        int indeksMyślnika = adres.IndexOf('-', i + 1);
+                        pierwszaCzęśćKoduPocztowego = Int32.Parse(adres.Substring(i, indeksMyślnika - i));
 
-                        for (int j = dashIndex + 1; j < adres.Length - 1; j++)
+                        for (int j = indeksMyślnika + 1; j < adres.Length - 1; j++)
                             if (!Char.IsNumber(adres[j + 1]))
                             {
-                                secondPartOfPostalCode = Int32.Parse(adres.Substring(dashIndex + 1, j - dashIndex + 1));
-                                cityIndex = j + 1;
-                                postalCodeFound = true;
+                                drugaCzęśćKoduPocztowego = Int32.Parse(adres.Substring(indeksMyślnika + 1, j - indeksMyślnika + 1));
+                                indeksMiasta = j + 1;
+                                kodPocztowyZnaleziony = true;
 
                                 break;
                             }
                     }
                     catch { }
 
-            if (postalCodeFound)
-                kod = String.Format("{0}-{1}", firstPartOfPostalCode, secondPartOfPostalCode);
+            if (kodPocztowyZnaleziony)
+                kod = String.Format("{0}-{1}", pierwszaCzęśćKoduPocztowego, drugaCzęśćKoduPocztowego);
             else
                 kod = null;
 
             try
             {
-                int commaIndex = adres.IndexOf(adres, cityIndex);
+                int indeksPrzecinka = adres.IndexOf(adres, indeksMiasta);
 
-                if (commaIndex == -1)
-                    commaIndex = Int32.MaxValue;
+                if (indeksPrzecinka == -1)
+                    indeksPrzecinka = Int32.MaxValue;
 
-                miasto = adres.Substring(cityIndex, new int[] { commaIndex, semicolonIndex, adres.Length }.Where(i => i > cityIndex).Min() - cityIndex).Trim();
+                miasto = adres.Substring(indeksMiasta, new int[] { indeksPrzecinka, indeksŚrednika, adres.Length }.Where(i => i > indeksMiasta).Min() - indeksMiasta).Trim();
             }
             catch
             {
@@ -198,14 +215,28 @@ namespace dbfToXml
             }
         }
 
-        static string BoolToPolishBool(object value)
+        static string ZamieńBoolNaPolskąNazwę(object wartość)
         {
-            bool boolValue = Convert.ToBoolean(value);
+            bool wartośćBoolowska = Convert.ToBoolean(wartość);
 
-            if (boolValue)
+            if (wartośćBoolowska)
                 return "Tak";
             else
                 return "Nie";
+        }
+
+        static void DodajPozycję(XmlNode rodzic, XmlNode wzór, decimal stawkaVat, string statusVat, decimal netto, decimal vat, string rodzajSprzedaży, bool uwzględnionoWProporcji)
+        {
+            XmlNode węzełPozycji = wzór.CloneNode(true);
+
+            węzełPozycji.ChildNodes[0].InnerText = stawkaVat.ToString();
+            węzełPozycji.ChildNodes[1].InnerText = statusVat.Trim();
+            węzełPozycji.ChildNodes[2].InnerText = węzełPozycji.ChildNodes[3].InnerText = węzełPozycji.ChildNodes[4].InnerText = netto.ToString();
+            węzełPozycji.ChildNodes[5].InnerText = vat.ToString();
+            węzełPozycji.ChildNodes[6].InnerText = rodzajSprzedaży.Trim();
+            węzełPozycji.ChildNodes[7].InnerText = ZamieńBoolNaPolskąNazwę(uwzględnionoWProporcji);
+
+            rodzic.AppendChild(węzełPozycji);
         }
     }
 }
