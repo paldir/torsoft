7e149519e329586860f00d7053fd39f6745fc8ba
Patryk <patrickus@wp.pl>
2015-05-25 13:49:39 +0200

    CXIX

--- czynsze/Formularze/KwotaCzynszu.aspx.cs
+++ czynsze/Formularze/KwotaCzynszu.aspx.cs
@@ -12,7 +12,7 @@ namespace czynsze.Formularze
         protected void Page_Load(object sender, EventArgs e)
         {
             Enumeratory.KwotaCzynszu tryb = PobierzWartośćParametru<Enumeratory.KwotaCzynszu>("tryb");
-            string zakres = Request.Params.AllKeys.FirstOrDefault(k => k.EndsWith("KwotaCzynszu"));
+            string zakres = Request.Params.AllKeys.FirstOrDefault(k => k.EndsWith("Wybór"));
             Start.ŚcieżkaStrony = new List<string>() { "Raporty", "Kwota czynszu" };
 
             switch (tryb)
@@ -35,77 +35,34 @@ namespace czynsze.Formularze
             using (DostępDoBazy.CzynszeKontekst db = new DostępDoBazy.CzynszeKontekst())
             {
                 IEnumerable<DostępDoBazy.AktywnyLokal> wszystkieLokale = db.AktywneLokale.OrderBy(l => l.kod_lok).ThenBy(l => l.nr_lok);
-                string minimalnyBudynek = (db.Budynki.Any() ? db.Budynki.Min(b => b.kod_1) : 0).ToString();
-                string minimalnyLokal = wszystkieLokale.First().nr_lok.ToString();
-                string maksymalnyBudynek = (db.Budynki.Any() ? db.Budynki.Max(b => b.kod_1) : 0).ToString();
-                string maksymalnyLokal = wszystkieLokale.Last().nr_lok.ToString();
-                string minimalnaWspólnota = (db.Wspólnoty.Any() ? db.Wspólnoty.Min(c => c.kod) : 0).ToString();
-                string maksymalnaWspólnota = (db.Wspólnoty.Any() ? db.Wspólnoty.Max(c => c.kod) : 0).ToString();
+                int minimalnyBudynek;
+                int minimalnyLokal;
+                int maksymalnyBudynek;
+                int maksymalnyLokal;
+                int minimalnaWspólnota;
+                int maksymalnaWspólnota;
 
                 if (String.IsNullOrEmpty(zakres))
                 {
-                    List<string[]> budynki = db.Budynki.OrderBy(b => b.kod_1).ToList().Select(b => new string[] { b.kod_1.ToString(), b.kod_1.ToString(), b.adres, b.adres_2 }).ToList();
-                    List<string[]> wspólnoty = db.Wspólnoty.OrderBy(w => w.kod).ToList().Select(w => new string[] { w.kod.ToString(), w.kod.ToString(), w.nazwa_skr, w.adres, w.adres_2 }).ToList();
-                    List<string[]> lokale = new List<string[]>();
-
-                    foreach (DostępDoBazy.AktywnyLokal lokal in wszystkieLokale)
-                    {
-                        string id = String.Format("{0}-{1}", lokal.kod_lok, lokal.nr_lok);
-
-                        lokale.Add(new string[] { id, id, lokal.adres, lokal.adres_2 });
-                    }
-
                     form.Controls.Add(new Kontrolki.HtmlInputHidden("tryb", tryb.ToString()));
 
-                    placeOfPlaces.Controls.Add(new Kontrolki.Button("button", "wszystkieLokaleKwotaCzynszu", "Zestawienie wszystkich lokali", String.Empty));
-                    DodajNowąLinię(placeOfPlaces);
-                    placeOfPlaces.Controls.Add(new Kontrolki.Button("button", "odDoLokaluKwotaCzynszu", "Od-do żądanego lokalu", String.Empty));
-                    DodajNowąLinię(placeOfPlaces);
-                    placeOfPlaces.Controls.Add(new Kontrolki.Label("label", "odLokalu", "Pierwszy lokal: ", String.Empty));
-                    placeOfPlaces.Controls.Add(new Kontrolki.DropDownList("field", "odLokalu", lokale, String.Format("{0}-{1}", minimalnyBudynek, minimalnyLokal), true, false));
-                    DodajNowąLinię(placeOfPlaces);
-                    placeOfPlaces.Controls.Add(new Kontrolki.Label("label", "doLokalu", "Ostatni lokal: ", String.Empty));
-                    placeOfPlaces.Controls.Add(new Kontrolki.DropDownList("field", "doLokalu", lokale, String.Format("{0}-{1}", maksymalnyBudynek, maksymalnyLokal), true, false));
-
-                    placeOfBuildings.Controls.Add(new Kontrolki.Button("button", "wszystkieBudynkiKwotaCzynszu", "Zestawienie wszystkich budynków", String.Empty));
-                    DodajNowąLinię(placeOfBuildings);
-                    placeOfBuildings.Controls.Add(new Kontrolki.Button("button", "odDoBudynkuKwotaCzynszu", "Od-do żądanego budynku", String.Empty));
-                    DodajNowąLinię(placeOfBuildings);
-                    placeOfBuildings.Controls.Add(new Kontrolki.Label("label", "odBudynku", "Pierwszy budynek: ", String.Empty));
-                    //placeOfBuildings.Controls.Add(new Kontrolki.TextBox("field", "odBudynku", minimalnyBudynek, Kontrolki.TextBox.TextBoxMode.LiczbaCałkowita, 5, 1, true));
-                    placeOfBuildings.Controls.Add(new Kontrolki.DropDownList("field", "odBudynku", budynki, minimalnyBudynek, true, false));
-                    DodajNowąLinię(placeOfBuildings);
-                    placeOfBuildings.Controls.Add(new Kontrolki.Label("label", "doBudynku", "Ostatni budynek: ", String.Empty));
-                    //placeOfBuildings.Controls.Add(new Kontrolki.TextBox("field", "doBudynku", maksymalnyBudynek, Kontrolki.TextBox.TextBoxMode.LiczbaCałkowita, 5, 1, true));
-                    placeOfBuildings.Controls.Add(new Kontrolki.DropDownList("field", "doBudynku", budynki, maksymalnyBudynek, true, false));
-
-                    placeOfCommunities.Controls.Add(new Kontrolki.Button("button", "wszystkieWspólnotyKwotaCzynszu", "Zestawienie wszystkich wspólnot", String.Empty));
-                    DodajNowąLinię(placeOfCommunities);
-                    placeOfCommunities.Controls.Add(new Kontrolki.Button("button", "odDoWspólnotyKwotaCzynszu", "Od-do żądanej wspólnoty", String.Empty));
-                    DodajNowąLinię(placeOfCommunities);
-                    placeOfCommunities.Controls.Add(new Kontrolki.Label("label", "odWspólnoty", "Pierwsza wspólnota: ", String.Empty));
-                    //placeOfCommunities.Controls.Add(new Kontrolki.TextBox("field", "odWpólnoty", minimalnaWspólnota, Kontrolki.TextBox.TextBoxMode.LiczbaCałkowita, 5, 1, true));
-                    placeOfCommunities.Controls.Add(new Kontrolki.DropDownList("field", "odWspólnoty", wspólnoty, minimalnaWspólnota, true, false));
-                    DodajNowąLinię(placeOfCommunities);
-                    placeOfCommunities.Controls.Add(new Kontrolki.Label("label", "doWspólnoty", "Ostatnia wspólnota: ", String.Empty));
-                    //placeOfCommunities.Controls.Add(new Kontrolki.TextBox("field", "doWspólnoty", maksymalnaWspólnota, Kontrolki.TextBox.TextBoxMode.LiczbaCałkowita, 5, 1, true));
-                    placeOfCommunities.Controls.Add(new Kontrolki.DropDownList("field", "doWspólnoty", wspólnoty, maksymalnaWspólnota, true, false));
+                    DodajWybórLokaliBudynkówIWspólnot(placeOfPlaces, placeOfBuildings, placeOfCommunities, out minimalnyBudynek, out minimalnyLokal, out maksymalnyBudynek, out maksymalnyLokal, out minimalnaWspólnota, out maksymalnaWspólnota);
                 }
                 else
                 {
-                    zakres = zakres.Substring(zakres.LastIndexOf('$') + 1).Replace("KwotaCzynszu", String.Empty);
-                    string kod_1_1, kod_1_2, nr1, nr2, kod1, kod2;
-                    kod_1_1 = kod_1_2 = nr1 = nr2 = kod1 = kod2 = "0";
+                    zakres = zakres.Substring(zakres.LastIndexOf('$') + 1).Replace("Wybór", String.Empty);
+                    int kod_1_1, kod_1_2, nr1, nr2, kod1, kod2;
+                    nr1 = nr2 = kod1 = kod2 = 0;
                     Enumeratory.Raport raport = (Enumeratory.Raport)(-1);
+                    kod_1_1 = PobierzWartośćParametru<int>("minimalnyBudynek");
+                    kod_1_2 = PobierzWartośćParametru<int>("maksymalnyBudynek");
 
                     switch (zakres)
                     {
                         case "wszystkieLokale":
                             raport = Enumeratory.Raport.KwotaCzynszuLokali;
-                            kod_1_1 = minimalnyBudynek;
-                            nr1 = minimalnyLokal;
-                            kod_1_2 = maksymalnyBudynek;
-                            nr2 = maksymalnyLokal;
+                            nr1 = PobierzWartośćParametru<int>("minimalnyLokal");
+                            nr2 = PobierzWartośćParametru<int>("maksymalnyLokal");
 
                             break;
 
@@ -113,38 +70,36 @@ namespace czynsze.Formularze
                             raport = Enumeratory.Raport.KwotaCzynszuLokali;
                             string[] odLokalu = PobierzWartośćParametru<string>("odLokalu").Split('-');
                             string[] doLokalu = PobierzWartośćParametru<string>("doLokalu").Split('-');
-                            kod_1_1 = odLokalu[0];
-                            nr1 = odLokalu[1];
-                            kod_1_2 = doLokalu[0];
-                            nr2 = doLokalu[1];
+                            kod_1_1 = Int32.Parse(odLokalu[0]);
+                            nr1 = Int32.Parse(odLokalu[1]);
+                            kod_1_2 = Int32.Parse(doLokalu[0]);
+                            nr2 = Int32.Parse(doLokalu[1]);
 
                             break;
 
                         case "wszystkieBudynki":
                             raport = Enumeratory.Raport.KwotaCzynszuBudynków;
-                            kod_1_1 = minimalnyBudynek;
-                            kod_1_2 = maksymalnyBudynek;
 
                             break;
 
                         case "odDoBudynku":
                             raport = Enumeratory.Raport.KwotaCzynszuBudynków;
-                            kod_1_1 = PobierzWartośćParametru<string>("odBudynku");
-                            kod_1_2 = PobierzWartośćParametru<string>("doBudynku");
+                            kod_1_1 = PobierzWartośćParametru<int>("odBudynku");
+                            kod_1_2 = PobierzWartośćParametru<int>("doBudynku");
 
                             break;
 
                         case "wszystkieWspólnoty":
                             raport = Enumeratory.Raport.KwotaCzynszuWspólnot;
-                            kod1 = minimalnaWspólnota;
-                            kod2 = maksymalnaWspólnota;
+                            kod1 = PobierzWartośćParametru<int>("minimalnaWspólnota");
+                            kod2 = PobierzWartośćParametru<int>("maksymalnaWspólnota");
 
                             break;
 
                         case "odDoWspólnoty":
                             raport = Enumeratory.Raport.KwotaCzynszuWspólnot;
-                            kod1 = PobierzWartośćParametru<string>("odWpólnoty");
-                            kod2 = PobierzWartośćParametru<string>("doWspólnoty");
+                            kod1 = PobierzWartośćParametru<int>("odWspólnoty");
+                            kod2 = PobierzWartośćParametru<int>("doWspólnoty");
 
                             break;
                     }
--- czynsze/Formularze/Strona.cs
+++ czynsze/Formularze/Strona.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.Web.UI;
+
 namespace czynsze.Formularze
 {
     public class Strona : System.Web.UI.Page
@@ -20,12 +22,12 @@ namespace czynsze.Formularze
                 return (T)Convert.ChangeType(wartość, typeof(T));
         }
 
-        public static void DodajNowąLinię(System.Web.UI.Control pojemnik)
+        public static void DodajNowąLinię(Control pojemnik)
         {
-            pojemnik.Controls.Add(new System.Web.UI.LiteralControl("<br />"));
+            pojemnik.Controls.Add(new LiteralControl("<br />"));
         }
 
-        public static void DodajWybórLokali(System.Web.UI.Control pojemnik, out int minimalnyBudynek, out int minimalnyLokal, out int maksymalnyBudynek, out int maksymalnyLokal)
+        public static void DodajWybórLokali(Control pojemnik, out int minimalnyBudynek, out int minimalnyLokal, out int maksymalnyBudynek, out int maksymalnyLokal)
         {
             using (DostępDoBazy.CzynszeKontekst db = new DostępDoBazy.CzynszeKontekst())
             {
@@ -50,6 +52,49 @@ namespace czynsze.Formularze
                 DodajNowąLinię(pojemnik);
                 pojemnik.Controls.Add(new Kontrolki.Label("label", "doLokalu", "Ostatni lokal: ", String.Empty));
                 pojemnik.Controls.Add(new Kontrolki.DropDownList("field", "doLokalu", lokaleDoListy, String.Format("{0}-{1}", maksymalnyBudynek, maksymalnyLokal), true, false));
+            }
+        }
+
+        public static void DodajWybórLokaliBudynkówIWspólnot(Control pojemnikLokali, Control pojemnikBudynków, Control pojemnikWspólnot, out int minimalnyBudynek, out int minimalnyLokal, out int maksymalnyBudynek, out int maksymalnyLokal, out int minimalnaWspólnota, out int maksymalnaWspólnota)
+        {
+            using (DostępDoBazy.CzynszeKontekst db = new DostępDoBazy.CzynszeKontekst())
+            {
+                List<string[]> budynki = db.Budynki.OrderBy(b => b.kod_1).ToList().Select(b => new string[] { b.kod_1.ToString(), b.kod_1.ToString(), b.adres, b.adres_2 }).ToList();
+                List<string[]> wspólnoty = db.Wspólnoty.OrderBy(w => w.kod).ToList().Select(w => new string[] { w.kod.ToString(), w.kod.ToString(), w.nazwa_skr, w.adres, w.adres_2 }).ToList();
+                minimalnaWspólnota = (db.Wspólnoty.Any() ? db.Wspólnoty.Min(c => c.kod) : 0);
+                maksymalnaWspólnota = (db.Wspólnoty.Any() ? db.Wspólnoty.Max(c => c.kod) : 0);
+
+                pojemnikLokali.Controls.Add(new Kontrolki.Button("button", "wszystkieLokaleWybór", "Zestawienie wszystkich lokali", String.Empty));
+                DodajNowąLinię(pojemnikLokali);
+                pojemnikLokali.Controls.Add(new Kontrolki.Button("button", "odDoLokaluWybór", "Od-do żądanego lokalu", String.Empty));
+                DodajNowąLinię(pojemnikLokali);
+                DodajWybórLokali(pojemnikLokali, out minimalnyBudynek, out minimalnyLokal, out maksymalnyBudynek, out maksymalnyLokal);
+                pojemnikLokali.Controls.Add(new Kontrolki.HtmlInputHidden("minimalnyLokal", minimalnyLokal.ToString()));
+                pojemnikLokali.Controls.Add(new Kontrolki.HtmlInputHidden("maksymalnyLokal", maksymalnyLokal.ToString()));
+
+                pojemnikBudynków.Controls.Add(new Kontrolki.Button("button", "wszystkieBudynkiWybór", "Zestawienie wszystkich budynków", String.Empty));
+                DodajNowąLinię(pojemnikBudynków);
+                pojemnikBudynków.Controls.Add(new Kontrolki.Button("button", "odDoBudynkuWybór", "Od-do żądanego budynku", String.Empty));
+                DodajNowąLinię(pojemnikBudynków);
+                pojemnikBudynków.Controls.Add(new Kontrolki.Label("label", "odBudynku", "Pierwszy budynek: ", String.Empty));
+                pojemnikBudynków.Controls.Add(new Kontrolki.DropDownList("field", "odBudynku", budynki, minimalnyBudynek.ToString(), true, false));
+                DodajNowąLinię(pojemnikBudynków);
+                pojemnikBudynków.Controls.Add(new Kontrolki.Label("label", "doBudynku", "Ostatni budynek: ", String.Empty));
+                pojemnikBudynków.Controls.Add(new Kontrolki.DropDownList("field", "doBudynku", budynki, maksymalnyBudynek.ToString(), true, false));
+                pojemnikBudynków.Controls.Add(new Kontrolki.HtmlInputHidden("minimalnyBudynek", minimalnyBudynek.ToString()));
+                pojemnikBudynków.Controls.Add(new Kontrolki.HtmlInputHidden("maksymalnyBudynek", maksymalnyBudynek.ToString()));
+
+                pojemnikWspólnot.Controls.Add(new Kontrolki.Button("button", "wszystkieWspólnotyWybór", "Zestawienie wszystkich wspólnot", String.Empty));
+                DodajNowąLinię(pojemnikWspólnot);
+                pojemnikWspólnot.Controls.Add(new Kontrolki.Button("button", "odDoWspólnotyWybór", "Od-do żądanej wspólnoty", String.Empty));
+                DodajNowąLinię(pojemnikWspólnot);
+                pojemnikWspólnot.Controls.Add(new Kontrolki.Label("label", "odWspólnoty", "Pierwsza wspólnota: ", String.Empty));
+                pojemnikWspólnot.Controls.Add(new Kontrolki.DropDownList("field", "odWspólnoty", wspólnoty, minimalnaWspólnota.ToString(), true, false));
+                DodajNowąLinię(pojemnikWspólnot);
+                pojemnikWspólnot.Controls.Add(new Kontrolki.Label("label", "doWspólnoty", "Ostatnia wspólnota: ", String.Empty));
+                pojemnikWspólnot.Controls.Add(new Kontrolki.DropDownList("field", "doWspólnoty", wspólnoty, maksymalnaWspólnota.ToString(), true, false));
+                pojemnikWspólnot.Controls.Add(new Kontrolki.HtmlInputHidden("minimalnaWspólnota", minimalnaWspólnota.ToString()));
+                pojemnikWspólnot.Controls.Add(new Kontrolki.HtmlInputHidden("maksymalnaWspólnota", maksymalnaWspólnota.ToString()));
             }
         }
     }
