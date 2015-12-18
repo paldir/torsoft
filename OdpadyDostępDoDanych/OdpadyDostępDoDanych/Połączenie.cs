75805dced1607f8e7ce132e7872addbce7b28c96
Patryk <patrickus@wp.pl>
2015-12-18 14:32:52 +0100

    ...

---
+++ OdpadyDostępDoDanych/OdpadyDostępDoDanych/Połączenie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FirebirdSql.Data.FirebirdClient;
+using System.Data;
+
+namespace OdpadyDostępDoDanych
+{
+    public class Połączenie
+    {
+        public Połączenie()
+        {
+            string parametry =
+            "User=SYSDBA;" +
+            "Password=masterkey;" +
+            @"Database=C:\Users\paldir\Documents\GitHub\torsoft\OdpadyDostępDoDanych\Testy\bin\Debug\odpady.fdb;" +
+            "DataSource=localhost;" +
+            "Port=3050;" +
+            "Dialect=3;" +
+            "Charset=NONE;" +
+            "Role=;" +
+            "Connection lifetime=15;" +
+            "Pooling=true;" +
+            "MinPoolSize=0;" +
+            "MaxPoolSize=50;" +
+            "Packet Size=8192;" +
+            "ServerType=0";
+
+            FbConnection połączenie = new FbConnection(parametry);
+
+            połączenie.Open();
+
+            FbDataAdapter adapter = new FbDataAdapter("SELECT * FROM address_list", połączenie);
+            DataSet zbiór = new DataSet();
+
+            adapter.Fill(zbiór);
+        }
+    }
+}
---
+++ OdpadyDostępDoDanych/Testy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OdpadyDostępDoDanych;
+
+namespace Testy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Połączenie p = new Połączenie();
+        }
+    }
+}
